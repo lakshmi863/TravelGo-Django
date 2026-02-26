02651f3e3aa8ab4be56bcc42f450ed070d8f9b23
lakshmi863 <sathya.lakshmipathi@gmail.com>
2026-02-26 07:42:43 +0530

    path add list

--- Backend/travelgo_django/settings.py
+++ Backend/travelgo_django/settings.py
@@ -123,7 +123,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 465               
-EMAIL_USE_TLS = True
+
 EMAIL_USE_SSL = True  
 EMAIL_HOST_USER = 'lalit.lakshmipathi@gmail.com'
 EMAIL_HOST_PASSWORD = 'uydn mijd uktm qsdh'
