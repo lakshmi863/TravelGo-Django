12c23c4283f422e62e9c39a403187e32f0526581
lakshmi863 <sathya.lakshmipathi@gmail.com>
2026-05-28 11:19:18 +0530

    setting change

--- Backend/travelgo_django/settings.py
+++ Backend/travelgo_django/settings.py
@@ -104,7 +104,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://travelgo-front.onrender.com",
+    "https://travel-go-ai-frontend.onrender.com",
 ]
 
 
